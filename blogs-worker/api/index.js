e04ec674d4b9c7f5df8a25e0849cf623266833c1
HridayDev <hridaykh1234@gmail.com>
2025-03-23 12:37:05 +0530

    Revert "sdfdsf"
    This reverts commit 20ad89f5e4335fd3e496477aeb9c8e9a8b74ae28.

--- blogs-worker/api/index.js
+++ blogs-worker/api/index.js
@@ -1,23 +1,13 @@
-import * as Sentry from "@sentry/cloudflare";
+export default {
+  fetch(request, env) {
+    const url = new URL(request.url);
 
-export default Sentry.withSentry(
-	() => ({
-		dsn: "https://43ab0940ee2e4178cab2bec3ca70bfe9@o4509022198431744.ingest.de.sentry.io/4509022212128848",
-		tracesSampleRate: 1.0, // Adjust to control how much tracing data to collect
-	}),
-	{
-		async fetch(request, env) {
-			const url = new URL(request.url);
+    if (url.pathname.startsWith("/api/")) {
+      return Response.json({
+        name: "Cloudflare",
+      });
+    }
 
-			if (url.pathname.startsWith("/api/")) {
-				throw new Error("Test error: Sentry setup complete!");;
-
-				return Response.json({
-					name: "Cloudflare",
-				});
-			}
-
-			return env.ASSETS.fetch(request);
-		},
-	}
-);
+    return env.ASSETS.fetch(request);
+  },
+}
--- blogs-worker/vite.config.js
+++ blogs-worker/vite.config.js
@@ -1,4 +1,3 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,12 +5,5 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare(), sentryVitePlugin({
-    org: "hriday-jg",
-    project: "blogs"
-  })],
-
-  build: {
-    sourcemap: true
-  }
+  plugins: [react(), cloudflare()],
 })
