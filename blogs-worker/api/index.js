20ad89f5e4335fd3e496477aeb9c8e9a8b74ae28
HridayDev <hridaykh1234@gmail.com>
2025-03-23 12:32:35 +0530

    sdfdsf

--- blogs-worker/api/index.js
+++ blogs-worker/api/index.js
@@ -1,13 +1,23 @@
-export default {
-  fetch(request, env) {
-    const url = new URL(request.url);
+import * as Sentry from "@sentry/cloudflare";
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
-    }
+export default Sentry.withSentry(
+	() => ({
+		dsn: "https://43ab0940ee2e4178cab2bec3ca70bfe9@o4509022198431744.ingest.de.sentry.io/4509022212128848",
+		tracesSampleRate: 1.0, // Adjust to control how much tracing data to collect
+	}),
+	{
+		async fetch(request, env) {
+			const url = new URL(request.url);
 
-    return env.ASSETS.fetch(request);
-  },
-}
+			if (url.pathname.startsWith("/api/")) {
+				throw new Error("Test error: Sentry setup complete!");;
+
+				return Response.json({
+					name: "Cloudflare",
+				});
+			}
+
+			return env.ASSETS.fetch(request);
+		},
+	}
+);
--- blogs-worker/vite.config.js
+++ blogs-worker/vite.config.js
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,5 +6,12 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), cloudflare(), sentryVitePlugin({
+    org: "hriday-jg",
+    project: "blogs"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
