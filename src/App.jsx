6b360a918383932cfd46f6f2b6d3f6e60e14c17e
HridayDev <hridaykh1234@gmail.com>
2025-03-25 15:28:23 +0530

    implement ract router basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 export default function App() {
 	return (
-		<>
-			{}
-		</>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="*" element={<NotFound />} />  {/* 404 Page */}
+			</Routes>
+		</Router>
 	);
+}
+
+function Home() {
+	return 
+		<div>
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/about">About</Link>
+			</nav>
+		</div>;
+}
+
+function About() {
+	return (
+		<div>
+			<h1>About Page</h1>
+			<p>Welcome to the About page!</p>
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/about">About</Link>
+			</nav>
+		</div>
+	);
+}
+
+function NotFound() {
+	return <h1>NotFound</h1>;
 }
